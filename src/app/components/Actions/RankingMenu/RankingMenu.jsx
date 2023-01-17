048ee26dca1fe1b4815ff2ee71a534cdaec1b42f
NicoooM <nmarsan33000@gmail.com>
2023-01-17 17:49:00 +0100

    [UPDATE] Grade icon ranking

--- src/app/components/Actions/RankingMenu/RankingMenu.jsx
+++ src/app/components/Actions/RankingMenu/RankingMenu.jsx
@@ -21,16 +21,28 @@ const RankingMenu = ({ isRankingModalActive }) => {
         <h2>Classement</h2>
         <ul className="user-list">
           {users
-            ? users.map((user) => (
-                <li
-                  key={user.uid + Math.floor(Math.random() * 10) + 1}
-                  className="user-card"
-                >
-                  <p>{user.username}</p>
-                  {"-"}
-                  <p>{user.totalScore}px</p>
-                </li>
-              ))
+            ? users.map((user) => {
+                let image;
+                const score = Math.floor(user.totalScore / 100) - 1;
+                if (score > 19) {
+                  image = `./grades/grade-19.jpg`;
+                } else {
+                  image = `./grades/grade-${score}.jpg`;
+                }
+                return (
+                  <li
+                    key={user.uid + Math.floor(Math.random() * 10) + 1}
+                    className="user-card"
+                  >
+                    <div className="user-card__img-container">
+                      <img src={image} alt="" />
+                    </div>
+                    <p>{user.username}</p>
+                    {"-"}
+                    <p>{user.totalScore}px</p>
+                  </li>
+                );
+              })
             : null}
         </ul>
       </div>
