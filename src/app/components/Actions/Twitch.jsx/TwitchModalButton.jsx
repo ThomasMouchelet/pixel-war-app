ea616aab0653bafde472648f1a31daafccb894f3
Fezard Léo <leo.fezard33@gmail.com>
2023-01-17 10:19:50 +0100

    [UPDATE] responsive

--- src/app/components/Actions/Twitch.jsx/TwitchModalButton.jsx
+++ src/app/components/Actions/Twitch.jsx/TwitchModalButton.jsx
@@ -42,14 +42,8 @@ const TwitchModalButton = ({hide}) => {
         <div
           className="action-menus__menu__item"
           onClick={() => setIsModalActive(false)}
-          style={{
-            zIndex: 12,
-            position: "fixed",
-            top: "10px",
-            right: "10px",
-          }}
         >
-          <img src={CloseIcon} alt="" className="menu" />
+          <img src={CloseIcon} alt="" className="menu action-menus_menu_item_cross" />
         </div>
       )}
 
