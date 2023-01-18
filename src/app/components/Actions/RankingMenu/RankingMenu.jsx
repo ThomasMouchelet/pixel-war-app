65e3be4a6231cc7344164bb073be4818589dca48
Draxx0 <williamfort.lmgl@gmail.com>
2023-01-18 16:13:59 +0100

    [ADD] ranking

--- src/app/components/Actions/RankingMenu/RankingMenu.jsx
+++ src/app/components/Actions/RankingMenu/RankingMenu.jsx
@@ -3,9 +3,11 @@ import { listenTopUser } from "../../../../setup/services/user.service";
 const RankingMenu = ({ isRankingModalActive }) => {
   const [users, setUsers] = useState(null);
   const [selfRank, setSelfRank] = useState(null);
+  const currentUser = localStorage.getItem("uid");
 
   useEffect(() => {
-    listenTopUser({setTopUsers: setUsers, setUserPosition: setSelfRank});
+    listenTopUser({ setTopUsers: setUsers, setUserPosition: setSelfRank });
+    console.log(selfRank);
   }, []);
   return (
     <div
@@ -28,10 +30,7 @@ const RankingMenu = ({ isRankingModalActive }) => {
                   image = `./grades/grade-${score}.jpg`;
                 }
                 return (
-                  <li
-                    key={user.uid + Math.floor(Math.random() * 10) + 1}
-                    className="user-rank-card"
-                  >
+                  <li key={index} className="user-rank-card">
                     <span className="user-rank">{index + 1}</span>
                     <div className="user-infos-container">
                       <div className="user-card__img-container">
@@ -49,33 +48,35 @@ const RankingMenu = ({ isRankingModalActive }) => {
         <div className="self-rank">
           <h2>Votre position</h2>
           <ul className="user-rank-list">
-          {selfRank
-            ? selfRank.map((user, index) => {
+            {selfRank &&
+              selfRank.map((user, index) => {
                 let image;
-                const score = Math.floor(user.totalScore / 100);
+                const score = Math.floor(user.user.totalScore / 100);
                 if (score > 19) {
                   image = `./grades/grade-19.jpg`;
                 } else {
                   image = `./grades/grade-${score}.jpg`;
                 }
-                return (
-                  <li
-                    key={user.i + Math.floor(Math.random() * 10) + 1}
-                    className="user-rank-card"
-                  >
-                    <span className="user-rank">{index + 1}</span>
+                return user.position !== 1 || user.position !== 2 ? (
+                  <li key={index} className=" user-self-rank-card">
+                    <span className="user-self-rank">{user.position}</span>
                     <div className="user-infos-container">
                       <div className="user-card__img-container">
                         <img src={image} alt="" />
                       </div>
-                      <span className="username">{user.username}</span>
-                      <span className="user-score">{user.totalScore} px</span>
+                      <span className="username">
+                        {currentUser === user.user.uid
+                          ? "Vous"
+                          : user.user.username}
+                      </span>
+                      <span className="user-score">
+                        {user.user.totalScore} px
+                      </span>
                     </div>
                   </li>
-                );
-              })
-            : null}
-        </ul>
+                ) : null;
+              })}
+          </ul>
         </div>
       </div>
     </div>
